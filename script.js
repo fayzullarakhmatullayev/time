17889082e20e6a1fde1984846be3f841c3462115
Fayzulla Rahmatullayev <80602071+fayzullarakhmatullayev@users.noreply.github.com>
2021-03-24 18:55:47 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
 
+function time(){
+    var date = new Date();
+    var hours = date.getHours();
+    var minutes = date.getMinutes().toString();
+    var seconds = date.getSeconds().toString();
+    
+    if(minutes.length == 1){
+        seconds = '0' + seconds;
+    }
+    if(seconds.length == 1){
+        seconds = '0' + seconds;
+    }
+    document.querySelector('.hours').innerHTML = hours;
+    document.querySelector('.minutes').innerHTML = ":"+minutes+":";
+    document.querySelector('.seconds').innerHTML = seconds;
+} 
+
+setInterval(() => {
+    time();
+}, 1000);
+
+
+
+
+
